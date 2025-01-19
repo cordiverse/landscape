b85a72fa42500f4f96fe21ef506dce8a83890608
Il Harper <hi@ilharper.com>
2025-01-19 18:13:24 +0800

    feat: add types

---
+++ packages/landscape/src/components/flow/types.ts
@@ -0,0 +1,27 @@
+import type {
+  Edge,
+  EdgeChange,
+  EdgeProps,
+  Node,
+  NodeChange,
+  NodeProps,
+} from '@xyflow/react'
+
+export interface CLNodeData {
+  label: string
+
+  [K: string]: unknown
+}
+
+export interface CLEdgeData {
+  primary?: boolean | undefined
+
+  [K: string]: unknown
+}
+
+export type CLNodeType = Node<CLNodeData>
+export type CLNodeChange = NodeChange<CLNodeType>
+export type CLNodeProps = NodeProps<CLNodeType>
+export type CLEdgeType = Edge<CLEdgeData>
+export type CLEdgeChange = EdgeChange<CLEdgeType>
+export type CLEdgeProps = EdgeProps<CLEdgeType>
