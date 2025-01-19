66af453647e0b479c3d990747caa0d51cb6235b0
Il Harper <hi@ilharper.com>
2025-01-20 01:40:54 +0800

    feat: add `About`

---
+++ packages/landscape/src/components/About.tsx
@@ -0,0 +1,34 @@
+import { Panel } from '@xyflow/react'
+import styles from './About.module.scss'
+
+export const About = () => (
+  <Panel position="bottom-left">
+    <div className={styles['text']}>
+      Cordis Landscape v{__DEFINE_CL_APP_VERSION_STRING__} |{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/ilharp/landscape"
+      >
+        GitHub
+      </a>{' '}
+      | Built with{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://reactflow.dev">
+        React Flow
+      </a>
+      {window.__CL_BEIAN__ && (
+        <>
+          {' '}
+          |{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://beian.miit.gov.cn/"
+          >
+            {window.__CL_BEIAN__}
+          </a>
+        </>
+      )}
+    </div>
+  </Panel>
+)
