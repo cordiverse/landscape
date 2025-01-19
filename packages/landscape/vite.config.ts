fb82575627187abb7446a5f8ec13b2aea2eec644
Il Harper <hi@ilharper.com>
2025-01-19 20:14:59 +0800

    feat: retrieve app version in vite config

--- packages/landscape/vite.config.ts
+++ packages/landscape/vite.config.ts
@@ -1,6 +1,51 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { SpawnOptions } from 'node:child_process'
+import { spawn } from 'node:child_process'
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
+// eslint-disable-next-line import/no-default-export
+export default defineConfig(async () => {
+  const [_, versionString] = await buildVersion()
+
+  return {
+    plugins: [react()],
+
+    define: {
+      __DEFINE_CL_APP_VERSION_STRING__: versionString,
+    },
+  }
 })
+
+const buildVersion = () =>
+  Promise.all([
+    spawnOutput('git', ['describe', '--tags', '--dirty']),
+    spawnOutput('git', ['rev-list', '--count', 'HEAD']),
+  ])
+    .then((x) => x.map((y) => y.trim()))
+    .then(([x, y]) => [y, `"${x} (${y})"`])
+    .catch(() => ['0', '"0.0.0 (0)"'])
+
+export async function spawnOutput(
+  command: string,
+  args?: ReadonlyArray<string>,
+  options?: SpawnOptions,
+): Promise<string> {
+  const parsedArgs = args ?? []
+  const parsedOptions: SpawnOptions = Object.assign<
+    SpawnOptions,
+    SpawnOptions,
+    SpawnOptions | undefined
+  >({}, { stdio: 'pipe', shell: true }, options)
+  const child = spawn(command, parsedArgs, parsedOptions)
+  let stdout = ''
+  if (!child.stdout)
+    throw new Error(`cannot get stdout of ${command} ${parsedArgs.join(' ')}`)
+  child.stdout.on('data', (x) => (stdout += x))
+  return new Promise<string>((resolve, reject) => {
+    child.on('close', (x) => {
+      // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+      if (x) reject(x)
+      else resolve(stdout)
+    })
+  })
+}
