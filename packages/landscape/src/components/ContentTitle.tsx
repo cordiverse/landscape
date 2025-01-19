d306e19020a6fb63bb3fa2792027a4d4a211accb
Il Harper <hi@ilharper.com>
2025-01-20 03:59:42 +0800

    feat: add `ContentTitle`

---
+++ packages/landscape/src/components/ContentTitle.tsx
@@ -0,0 +1,26 @@
+import styles from './ContentTitle.module.scss'
+
+export interface ContentTitleProps {
+  id: string
+  title: string
+  href: string
+  icon?: boolean | undefined
+}
+
+export const ContentTitle = ({ id, title, icon, href }: ContentTitleProps) => (
+  <div className={styles['container']}>
+    <h1 className={styles['title']}>
+      <a target="_blank" rel="noopener noreferrer" href={href}>
+        {title}
+      </a>
+    </h1>
+    {icon && (
+      <>
+        <div className={styles['separator']} />
+        <a target="_blank" rel="noopener noreferrer" href={href}>
+          <img className={styles['icon']} src={`/${id}.png`} />
+        </a>
+      </>
+    )}
+  </div>
+)
